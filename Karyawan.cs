aba61bcf1c8896ebfb26432f384fc496422f091e
Ghanim14 <ganimramadhan01@gmail@students.amikom.ac.id>
2021-04-13 15:21:09 +0700

    Finished

---
+++ Karyawan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiProgram3412
+{
+    class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            Nik = nik;
+            Nama = nama;
+
+            if (gaji < 0)
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = gaji;
+            }
+
+        }
+        public void shownKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void naikgaji()
+        {
+            int gaji = 0;
+            gaji = 10 * GajiBulanan / 100;
+            GajiBulanan += Convert.ToInt32(gaji);
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiProgram3412
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Karyawan1 = new Karyawan(190302123, "Paijo", 5000000);
+            Karyawan Karyawan2 = new Karyawan(190302124, "Jono", 1000000);
+
+            Console.WriteLine("Nik \t  \t Nama \t Gaji");
+            Console.WriteLine("------------------------------------------");
+            Karyawan1.shownKaryawan();
+            Karyawan2.shownKaryawan();
+
+            Console.WriteLine("\n Selamat Anda mendapatkan kenaikan gaji 10% \n");
+
+            Console.WriteLine("Nik \t  \t Nama \t Gaji");
+            Console.WriteLine("------------------------------------------");
+            Karyawan1.naikgaji();
+            Karyawan2.naikgaji();
+
+
+            Console.ReadKey();
+        }
+    }
+}
